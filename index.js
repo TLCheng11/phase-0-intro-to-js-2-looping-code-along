8932e27a3e86cab2fd9b159606756d9c4e3ce60a
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-05-12 16:30:05 -0400

    commit tree d35ac46fae5f56be02236ee3b336945c2f0dc4c0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, word) {
+    const result = [];
+    for (let i = 0; i < name.length; i++) {
+        result.push(`Thank you, ${name[i]}, for the wonderful ${word} gift!`);
+    }
+    return result;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
